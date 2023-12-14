7a92a66be43da5097acb07dd4069bbed73a66dd4
ckeller22 <chris.keller.dev@gmail.com>
2023-12-14 13:56:57 -0600

    feat: set up linting/prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,14 +2,32 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
     'plugin:react-hooks/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
   plugins: ['react-refresh'],
   rules: {
+    'comma-dangle': 'off',
+    '@typescript-eslint/comma-dangle': ['error', 'only-multiline'],
+    'arrow-body-style': ['error', 'always'],
+    // Default props in functional components will be deprecated "eventually"
+    'react/require-default-props': [
+      'error',
+      { ignoreFunctionalComponents: true },
+    ],
+    'react/react-in-jsx-scope': 0,
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
